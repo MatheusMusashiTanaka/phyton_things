e8fb5cb2a2d02d0e2c4c04ea62316cec069f6e6e
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-04-04 18:44:15 -0300

    Update aula_1_codes.py

--- aula_1_codes.py
+++ aula_1_codes.py
@@ -6,13 +6,19 @@ print(palavra_1)
 print(palavra_2)
 
 __________________________________________________________________
+
+
 nome = input('digite nome:')
 numero_de_letras = len(nome)
 print(numero_de_letras)
 __________________________________________________________________
+
+
 nome =input('digite nome: ') ; nome_invertido = str(nome[::-1]).upper()
 print (nome_invertido)
 ___________________________________________________________________
+
+
 nome = input('digite nome:')
 
 palavras = nome.split(' ')
@@ -20,9 +26,26 @@ palavras = nome.split(' ')
 print(len(palavras))
 print(palavras)
 ___________________________________________________________________
+
+
 nota = float(input("nota: "))
 if nota >= 7:
     print("aprovado")
 else:
     print("reprovado")
 ___________________________________________________________________    
+
+
+import random
+numero = int(input("digite um numero de 1 a 10: "))
+
+numero_premiado = random.randint(1,10)
+
+while numero != 0:
+    if numero_premiado == numero:
+        print("Venceu!!")
+        break
+    else:
+        print("tente novamente")
+        numero = int(input("digite um numero de 1 a 10 (digite 0 para parar): "))
+    
