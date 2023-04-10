905fdd7cb3aa786203312f382284264ee2ed6e11
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-04-10 19:03:46 -0300

    Update aula 3.py

--- aula 3.py
+++ aula 3.py
@@ -1,1 +1,129 @@
+#tarefa 1
+i = 1
+
+while i<11:
+    print(i)
+    i+=1
+__________________________________________________________
+#tarefa 2
+
+i = 2
+while i <= 100:
+    print(i)
+    i= i + 2
+__________________________________________________________  
+#tarefa 3
+n=10
+while n >=0:
+  print(n)
+  n+=1
+___________________________________________________________
+#tarefa 4
+n = int(input("numero: "))
+x=1
+while<=10:
+  result = x*n
+  print(result)
+  x+=1
+ _________________________________________________________
+#tarefa 5
+n=int(input("num: "))
+x = n
+while n!= 0:
+  print(x)
+  n=int(input("num: "))
+ _______________________________________ 
+#tarefa 6
+num = int(input("numero: "))
+
+while num != 0:
+    if (num % 2) == 0:
+        print("par")
+    elif (num % 2) != 0:
+        print("impar")
+    
+    num = int(input("numero (digite 0 para sair): "))
+________________________________________
+#tarefa 7
+i = 1
+
+while i <= 5:
+    numero = int(input("numero: "))
+    if numero < 0:
+        print("numero é negativo")
+    elif numero > 0:
+        print("numero é positivo")
+    i+=1
+ _______________________________________
+#tarefa 10
+i = 1
+soma=0
+while i <= 100:
+    soma += i
+    i+=1
+print (soma)
+    
+________________________________________
+#tarefa 11
+i=1
+soma=0
+while i <= 10:
+    numero=int(input("digite numero: "))
+    soma+=numero
+    i+=1
+print (f"soma :{soma}")
+________________________________________
+#tarefa 12
+ i=0
+soma=0
+while i < 1000:
+    soma+=i
+    i+=5
+print(soma)
+________________________________________
+#tarefa 13
+soma = 0
+while True:
+    if soma >=1000:
+        break
+    numero = int(input("digite: "))
+    soma += numero
+print(soma)
+________________________________________
+#tarefa 14
+________________________________________
+#tarefa 15
+
+soma = 0
+while True:
+    numero = int(input("numero: "))
+    if numero < 0:
+        break
+    soma += numero
+print(soma)
+_________________________________________
+ #tarefa 17
+  soma = 0
+i=1
+
+while i<=10:
+    numero = int(input("numero: "))
+    if numero < 0:
+        soma+=1
+    i+=1
+print (f"foram digitados :{soma} numeros negativos")
+print (soma)
+________________________________________
+#tarefa 18
+numero = int(input("Numero: "))
+divisores = 0
+i = numero
+while i > 0:
+    if (numero % i) == 0:
+        divisores+=1
+    i-=1
+print (divisores)
+_______________________________________
+#19
+
 
