37d8184af26384737eb4d9716df359f3159d65c8
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-09-11 09:42:45 -0300

    Create revisao.py

---
+++ revisao.py
@@ -0,0 +1,74 @@
+# criar uma lista de inteiro e inserir n elementos
+# inverter a lista de internos
+# retornar o produto dos elementos da lista criada
+# imprimir a lista
+# criar um dicionário de n alunos com as respectivas notas
+# imprimir os alunos com suas notas
+# alterar a nota de um aluno do dicionário
+# teclar <sair> ou <SAIR> para abandonar o programa
+
+import os
+import time
+
+
+def menu():
+    while True:
+        os.system("cls")
+        print("*****   M E N U   *****")
+        print("1. criar uma lista de inteiro e inserir n elementos")
+        print("2. inverter a lista de internos")
+        print("3. retornar o produto dos elementos da lista criada")
+        print("4. imprimir a lista")
+        print("5. criar um dicionário de n alunos com as respectivas notas")
+        print("6. imprimir os alunos com suas notas")
+        print("7. alterar a nota de um aluno do dicionário")
+        print("8. retornar uma lista dos elementos repetidos de duas listas - (obter as duas listas)")
+        print("teclar <sair> ou <SAIR> para abandonar o programa")
+
+        op = input("digite a opção: ")
+        if op == 'sair' or int(op) == 0:
+            break
+
+        if int(op) == 1:
+            lst = crialista()
+        if int(op) == 2:
+            l = lst.reverse
+        if int(op) == 3:
+            pass
+        if int(op) == 4:
+            imprimirlista(lst)
+        if int(op) == 5:
+            dic = criaDic()
+        if int(op) == 6:
+            imprimirDic(dic)
+def crialista():
+    l = []
+    x = int(input("quantos elementos pra lista? "))
+    for i in range(x):
+        elemento = int(input("digite o {} elemento da lista".format(i+1)))
+        l.append(elemento)
+    return l
+
+
+def imprimirlista(lista):
+    print(lista)
+    time.sleep(5)
+
+def criaDic():
+    dic={}
+    x = int(input("elementos dicionario:"))
+    for i in range(x):
+        nome = input("nome:")
+        nota = float(input("digite nota:"))
+        dic.get(nome)
+        dic[nome] = nota
+    return dic
+
+def imprimirDic(dic):
+    print(dic)
+    time.sleep(4)
+# programa principal
+
+def invert(lista)
+
+menu()
